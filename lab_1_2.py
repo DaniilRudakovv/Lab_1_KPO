c16c1c92392d1b687a6f1480013981dd9b739d11
DaniilRudakovv <pupokhappyboggg@gmail.com>
2023-02-19 18:32:14 +0300

    lab_1_2.py

--- lab_1_2.py
+++
@@ -1,9 +0,0 @@
-num = int(input())
-n = list(map(int, str(num)))
-total = 0
-while int(num) > 9:
-    total += 1
-    n = list(map(str, str(sum(n))))
-    num = ''.join(n)
-    n = list(map(int, n))
-print(total)
